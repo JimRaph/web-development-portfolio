0c8fd790a15e8476dfa5ee61d561bc89fc526a92
JimRaph <jimmyesang@gmail.com>
2025-01-25 01:00:53 +0100

    Log and Reg page logo

--- client/src/pages/onboarding/registration.jsx
+++ client/src/pages/onboarding/registration.jsx
@@ -37,10 +37,22 @@ const Registration = () => {
                 :(
                     <section className="bg-gray-50 dark:bg-gray-900">
                 <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-                    <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                        <img className=" h-8 mr-2" src="../src/assets/logo.png" alt="logo" />
-                            
-                    </a>
+                    {/* <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"> */}
+                        {/* <img className=" h-8 mr-2" src="../src/assets/logo.png" alt="logo" /> */}
+                        <Link to = "/" 
+              className="flex mb-3 items-center justify-around rtl:space-x-reverse"
+            >
+              {/* <img
+                src="../src/assets/logo.png"
+                className="h-8"
+                alt="Luxury & Cheap Logo"
+              /> */}
+              <span className="font-extrabold text-blue-700 p-1">Luxury & Cheap</span>
+              <span className="self-center h-9 text-2xl font-bold whitespace-nowrap text-blue-700">
+                |
+              </span>
+            </Link>    
+                    {/* </a> */}
                     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
