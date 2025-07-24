f3f9b2724d388d84960ef5758078cdc7ff41f6ff
JimRaph <jimmyesang@gmail.com>
2025-07-24 01:36:21 +0100

    variables changed

--- Inventory-Management-Dashboard/server/src/index.ts
+++ Inventory-Management-Dashboard/server/src/index.ts
@@ -22,8 +22,7 @@ app.use(morgan("common"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cors({
-    //  origin: 'http://localhost:3000'
-     origin: 'https://inventory-management-dashboard-virid.vercel.app'
+     origin: process.env.CLIENT_URL
 }))
 
 
