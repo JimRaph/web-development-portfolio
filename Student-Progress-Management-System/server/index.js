abbdfe22b8eeeeb8d19f636be899a4fa50db9d17
JimRaph <jimmyesang@gmail.com>
2025-07-26 16:05:24 +0100

    serverless

--- Student-Progress-Management-System/server/index.js
+++ Student-Progress-Management-System/server/index.js
@@ -10,44 +10,55 @@ import {syncAllStudentsSubmissions} from './utils/seedCfProblemStat.js';
 import { seedSyncConfig } from './utils/seedSyncConfig.js';
 import { startSyncCron } from './cron/syncJob.js';
 import syncRouter from './routes/syncConfigRoutes.js';
+import ServerlessHttp from 'serverless-http';
 
 dotenv.config()
 
-const app = express()
-app.use(cors({
-    origin: process.env.CLIENT_URL
+let server;
+
+const start_server = async()=>{
+
+  const app = express()
+  app.use(cors({
+      origin: process.env.CLIENT_URL
+  }
+  ))
+  app.use(express.json())
+
+  app.use('/api/students', studentRouter);
+  app.use('/api', syncRouter)
+
+  //connects to db
+  await mongoose_connector();
+
+  //seed database schemas
+
+  // await seedStudentsFromRatedList()
+  // await syncAllStudentsContestHistory()
+  // await syncAllStudentsSubmissions()
+  // console.log('calling seedSyncConfig');
+  // await seedSyncConfig();
+  console.log('calling startsynccron')
+  // await startSyncCron();
+
+  // startCronJob();
+
+  if (process.env.NODE_ENV !== 'production') {
+    const PORT = process.env.PORT || 3001;
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  }
+      
+  return ServerlessHttp(app)
+
 }
-))
-app.use(express.json())
 
-app.use('/api/students', studentRouter);
-app.use('/api', syncRouter)
-const PORT = process.env.PORT || 3001;
-
-//connects to db
-await mongoose_connector();
-
-//seed database schemas
-
-// await seedStudentsFromRatedList()
-// await syncAllStudentsContestHistory()
-// await syncAllStudentsSubmissions()
-// console.log('calling seedSyncConfig');
-// await seedSyncConfig();
-console.log('calling startsynccron')
-// await startSyncCron();
-
-// startCronJob();
-
-if (process.env.NODE_ENV !== 'production') {
-  const PORT = process.env.PORT || 3001;
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-  });
-}
-    
-const handler = async(req, res) => {
-  return app(req, res); 
+const handler = async(req, res) =>{
+  if(!server){
+    server = await start_server()
+  }
+  return server(req, res)
 }
 
 export default handler
